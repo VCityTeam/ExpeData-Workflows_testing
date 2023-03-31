57fda29c9ae5b01ffe1344f84d23bdf3a5860d03
EricBoix <eric.boix@insa-lyon.fr>
2023-03-31 19:17:14 +0200

    Forgotten parameters file.

---
+++ ArgoWorkflows/With_Hera/Workflow_CityGMLto3DTiles_Example/input_2012_tiny_import_dump.py
@@ -0,0 +1,9 @@
+import types
+
+parameters = types.SimpleNamespace(
+    borough="LYON_8EME",
+    pattern="BATI",
+    experiment_output_dir="junk",
+    vintage="2012",
+    persistedVolume="/within-container-mount-point",
+)

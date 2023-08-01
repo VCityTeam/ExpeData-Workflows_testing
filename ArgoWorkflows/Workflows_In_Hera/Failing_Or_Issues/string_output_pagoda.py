1405795d2df30d545f52c1939bd6c21c18b0a79b
EricBoix <eric.boix@insa-lyon.fr>
2023-08-01 17:41:06 +0200

    Failing example upgraded to 5.6.0

--- ArgoWorkflows/Workflows_In_Hera/Failing_Or_Issues/string_output_pagoda.py
+++ ArgoWorkflows/Workflows_In_Hera/Failing_Or_Issues/string_output_pagoda.py
@@ -7,40 +7,39 @@ from pagoda_cluster_definition import define_cluster
 
 cluster = define_cluster()
 
+from hera_utils import hera_assert_version
+
+hera_assert_version("5.6.0")
+
 ####################################
 # Following code submitted to Hera: refer to
-# https://github.com/argoproj-labs/hera/issues/559
+# https://github.com/argoproj-labs/hera/discussions/561
 ####################################
 
-from hera import Parameter, Task, Workflow
+from hera.workflows import DAG, Parameter, script, Task, Workflow
 
+
+@script()
 def dummy():
     ...
 
+
+@script()
 def consume(msg: str):
     print(f"Message was: {msg}")
 
-### The following workflow tries to transmit a simple string from
-# task t1 (as output) to task t2 (as input):
 
-with Workflow("io-", generate_name=True) as w:
-    # The following task will fail at submission stage with
-    #    templates.d.outputs.parameters.msg.path must be specified for 
-    #    Script templates
-    t1 = Task(
-        "d", dummy, outputs=[Parameter("msg", value="some_valuable_message")]
-    )
-    t2 = Task("c", consume, args={"msg": t1.get_parameter("msg")})
+with Workflow(generate_name="io-") as w:
+    with DAG(name="main"):
+        t1: Task = dummy(
+            name="t1",
+            # Ditch attempt to piggyback some message in the outputs, when
+            # the original template didn't have any output (don't ask about
+            # the use case since I have to admit it's quite circumvolved).
+            outputs=[Parameter(name="msg", value="some_valuable_message")],
+        )
+        t2: Task = consume(arguments={"msg": t1.get_parameter("msg")})
 
-    t1 >> t2
+        t1 >> t2
 
 w.create()
-
-# Note: defining task t2 as 
-#    t2 = Task("c", consume, inputs=[t1.get_parameter("msg")])
-# also fails at submission stage with
-#    templates.d.outputs.parameters.msg.path must be specified for 
-#    Script templates
-# Note that using inputs instead of args is a bit broader context because
-# "inputs" can be flown to the container parameters whereas "args" can only
-# be flown to the command standing in the container.
